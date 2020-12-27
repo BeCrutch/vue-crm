a94b1581df3f17827d50f61d265a42bbb02cb76d
BeCrutch <roman.naumenko.94@gmail.com>
2020-12-28 02:41:00 +0300

    lesson 2

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
   <div id="app">
-
-    <!-- Роутит все страницы в  router-view/>-->
-    <router-view/>
+    <component :is="layout">
+      <!-- Роутит все страницы в  router-view/>-->
+      <router-view />
+    </component>
   </div>
 </template>
 
+<script>
+import EmptyLayout from "@/layouts/EmptyLayout"
+import MainLayout from "@/layouts/MainLayout"
+  export default {
+    computed: {
+      layout() {
+        return (this.$route.meta.layout ||  'empty') + '-layout';
+      }
+    },
+    components: {
+      EmptyLayout, MainLayout
+    }
+  }
+</script>
+
 <style lang="scss">
+
+@import '~materialize-css/dist/css/materialize.min.css';
+@import 'assets/index.css';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
